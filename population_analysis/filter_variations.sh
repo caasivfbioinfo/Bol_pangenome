37911d131b36f3b9936cd16bd5f5f250f3a9ab42
caasivfbioinfo <151805058+caasivfbioinfo@users.noreply.github.com>
2023-11-24 15:20:03 +0800

    Delete population_analysis directory

--- population_analysis/filter_variations.sh
+++
@@ -1,5 +0,0 @@
-#only keep SNPs
-vcftools --gzvcf merge.vcf.gz --remove-indels --max-missing 0.5 --mac 3 --minQ 30 --minDP 3 --maf 0.05 --recode  --out merge.filtered.SNP
-
-#only keep InDels
-vcftools --gzvcf merge.vcf.gz --keep-only-indels  --max-missing 0.5 --mac 3 --minQ 30 --minDP 3 --maf 0.05 --recode  --out merge.filtered.InDel
